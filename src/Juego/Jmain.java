1442517f2c7bfffa5d12e07f4623fd449ad76901
Jose Antonio Espinoza <joseantonioestudiante2001@gmail.com>
2020-06-07 19:08:52 -0600

    Borrar Lineas Vacías

--- src/Juego/Jmain.java
+++ src/Juego/Jmain.java
@@ -11,7 +11,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
-
 /**
  * Jmain
  *Esta clase es la que ejecuta la ventana donde se aloja el juego
@@ -19,10 +18,8 @@ import java.util.concurrent.TimeUnit;
 
  */
 public class Jmain extends JFrame implements ActionListener {
-
-
     public JFrame frm2;
-    public JLabel bg2, cnp1, cnp2, cnp3, cnp4, str1, str2, str3, str4, round, pgame, mario, luigi, toad, yoshi, dado1, dado2, estrella;
+    public JLabel bg2, cnp1, cnp2, cnp3, cnp4, str1, str2, str3, str4, round, pgame, mario, luigi, toad, yoshi, dado1, dado2, estrella, preronda, preturno;
     public Casilla casillaActual;
     public JButton btnmkt, btndados, avanzar;
     public boolean corriendoJuego;
@@ -37,7 +34,6 @@ public class Jmain extends JFrame implements ActionListener {
      * Sinleton para Jmain
      */
     private static Jmain instance = null;
-
     /**
      * Jmain
      * Este constructor crea un frame, un panel y aloja objetos en ellos
@@ -45,13 +41,9 @@ public class Jmain extends JFrame implements ActionListener {
      * @authors Mauricio C. , Naheem Johnson , Jose Espinoza
      */
     public Jmain() {
-
         //String filepath = "maintheme.wav";
-
         //musicstuff musicObject = new musicstuff();
         //musicObject.playMusic(filepath);
-
-
         this.casillaActual = null;
         Map mapa = Map.getInstance();
         //EventStack pilaEv = EventStack.getInstance();
@@ -74,7 +66,9 @@ public class Jmain extends JFrame implements ActionListener {
         SLabelList.add(str2);
         SLabelList.add(str3);
         SLabelList.add(str4);
+
         //############################Frame###############################
+
         frm2 = new JFrame("Datos Party 1");
         frm2.setBounds(0, 0, 1295, 947);
         frm2.setTitle("Datos Party 1");
@@ -82,18 +76,19 @@ public class Jmain extends JFrame implements ActionListener {
         frm2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         //############################Panel###############################
+
         panel4 = new JPanel();
         panel4.setLayout(null);
         panel4.setSize(1330, 947);
         frm2.add(panel4);
 
         //############################Botones###############################
+
         avanzar = new JButton("Avanzar");
         avanzar.setBounds(1030, 609, 100, 50);
         avanzar.setVisible(false);
         avanzar.addActionListener(this);
         panel4.add(avanzar);
-
         btnmkt = new JButton();
         btnmkt.setBounds(1184, 811, 80, 80);
         ImageIcon btnurl7 = new ImageIcon(getClass().getResource("/Juego/carrito.png"));
@@ -103,7 +98,6 @@ public class Jmain extends JFrame implements ActionListener {
         btnmkt.setOpaque(false);
         btnmkt.addActionListener(this);
         panel4.add(btnmkt);
-
         btndados = new JButton("TIRAR DADOS");
         btndados.setBounds(870, 700, 385, 82);
         ImageIcon btnurl12 = new ImageIcon(getClass().getResource("/Juego/roll.png"));
@@ -112,11 +106,11 @@ public class Jmain extends JFrame implements ActionListener {
         panel4.add(btndados);
 
         //############################Labels###############################
+
         textmovimientos = new JTextField();
         textmovimientos.setBounds(950, 609, 50, 50);
         textmovimientos.setVisible(false);
         panel4.add(textmovimientos);
-
 
         //##Stars##
 
@@ -170,17 +164,22 @@ public class Jmain extends JFrame implements ActionListener {
         cnp4.setBounds(1187, 305, 80, 40);
         panel4.add(cnp4);
 
-
-        pgame = new JLabel("Turn of Player");
+        pgame = new JLabel("Turn of Player: ");
         pgame.setFont(new Font("Serif", Font.BOLD, 30));
         pgame.setForeground(Color.WHITE);
         pgame.setBounds(867, 830, 400, 40);
         panel4.add(pgame);
 
-        round = new JLabel("Round: 0");
+        preturno = new JLabel("IMAGEN");
+        preturno.setFont(new Font("Serif", Font.BOLD, 30));
+        preturno.setForeground(Color.WHITE);
+        preturno.setBounds(1100, 800,80,80);
+        panel4.add(preturno);
+
+        round = new JLabel("0");
         round.setFont(new Font("Serif", Font.BOLD, 30));
         round.setForeground(Color.WHITE);
-        round.setBounds(867, 795, 400, 40);
+        round.setBounds(980, 795, 400, 40);
         panel4.add(round);
 
         estrella = new JLabel("star");
@@ -188,6 +187,13 @@ public class Jmain extends JFrame implements ActionListener {
         ImageIcon fotostar = new ImageIcon(getClass().getResource("/Juego/estrella.png"));
         estrella.setIcon(fotostar);
         panel4.add(estrella);
+
+        preronda = new JLabel("Round: ");
+        preronda.setFont(new Font("Serif", Font.BOLD, 30));
+        preronda.setForeground(Color.WHITE);
+        preronda.setBounds(867, 795, 400, 40);
+        panel4.add(preronda);
+
         //###########################Jugadores##########################################
 
         mario = new JLabel();
@@ -198,7 +204,6 @@ public class Jmain extends JFrame implements ActionListener {
         panel4.add(mario);
         validate();
 
-
         luigi = new JLabel();
         luigi.setBounds(697, 781, 50, 50);
         ImageIcon bgurl36 = new ImageIcon(getClass().getResource("/Juego/P2.png"));
@@ -206,7 +211,6 @@ public class Jmain extends JFrame implements ActionListener {
         luigi.setVisible(true);
         panel4.add(luigi);
         validate();
-
 
         toad = new JLabel();
         toad.setBounds(697, 781, 50, 50);
@@ -225,6 +229,7 @@ public class Jmain extends JFrame implements ActionListener {
         validate();
 
         //#####################DADOS#####################################################
+
         dado1 = new JLabel();
         dado1.setBounds(889, 432, 160, 160);
         ImageIcon imagendado = new ImageIcon(getClass().getResource("/Juego/dadoroll1.png"));
@@ -236,19 +241,16 @@ public class Jmain extends JFrame implements ActionListener {
 
         panel4.add(dado1);
         panel4.add(dado2);
+
         //##Background####################################################################
+
         bg2 = new JLabel();
         bg2.setBounds(0, 0, 1280, 900);
         ImageIcon bgurl1 = new ImageIcon(getClass().getResource("/Juego/Jfondo.png"));
         bg2.setIcon(bgurl1);
         validate();
-
-
         panel4.add(bg2);
-
         frm2.setVisible(true);
-
-
     }
 
 
@@ -311,6 +313,7 @@ public class Jmain extends JFrame implements ActionListener {
     }
 
     ////////////////////////////////////////////////////MANEJO DE TURNOS////////////////////////////////////////////////////////////////////
+
     public void turns() {
         if (!corriendoJuego) {
             for (int i = 0; i < PlayerList.getLength(); i++) {
@@ -340,11 +343,14 @@ public class Jmain extends JFrame implements ActionListener {
     }
 
     ////////////////////////////////////////////////////////LANZA DADO////////////////////////////////////////////////////////////////
+
     public void lanzarDado(){
         corriendoJuego = true;
         dado.tm.start();
     }
+
     ///////////////////////////////////////////////////////METODOS DE ACTUALIZACIÓN DE OBJETOS////////////////////////////////////
+
     public void actualizarlistasMS(){
         for(int i = 0; i < PlayerList.getLength(); i++){
             CoinList.add(Integer.toString(castToPlayer(PlayerList.getPos(i)).monedas));
@@ -383,21 +389,25 @@ public class Jmain extends JFrame implements ActionListener {
     }
 
     ////////////////////////////////////////////////////////////COLOCA EL LABEL EN LAS POSICIONES DE LA CLASE ESTRELLA///////////////////////
+
     public void colocarEstrella(){
         estrella.setLocation(star.posX,star.posY);
     }
 
     ///////////////////////////////////////////////////////////RETORNA LA LISTA DE LOS JUGADORES////////////////////////////////////////////
+
     public SimpleList getPlayerList(){
         return PlayerList;
     }
 
     //////////////////////////////////////////////////////////RETORNA UN JUGADOR ALEATORIO DE LA LISTA/////////////////////////////////////
+
     public Player getRandomPlayer(){
         int pos = new Random().nextInt(PlayerList.getLength());
         return (Player)PlayerList.getPos(pos);
     }
     //////////////////////////////////////SINGLETON DE JMAIN//////////////////////////////////////////////////////////////////////////////
+
     public static Jmain getInstance(){
         if(instance == null){
             instance = new Jmain();
@@ -421,16 +431,11 @@ public class Jmain extends JFrame implements ActionListener {
 
         if (e.getSource() == avanzar) {
             Jmain.getInstance().playing.movePlayer(Integer.parseInt(textmovimientos.getText()));
-
-
         }
-
         if (e.getSource() == btnmkt) {
             System.out.println("Abriendo Market");
             System.out.println("Market abierto");
             market prueba1 = new market();
-
-
         }
 
         if (e.getSource() == btndados) {
@@ -439,7 +444,6 @@ public class Jmain extends JFrame implements ActionListener {
             System.out.println("Se estan tirando los dados");
             System.out.println("Dados tirados");
             turns();
-
         }
         }
     }
