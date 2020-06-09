f1e443619c62ab312bcd7194d203740875524db3
MAU <mau141226@gmail.com>
2020-06-09 01:17:50 -0600

    avance eventos

--- src/Eventos/StealCoins.java
+++ src/Eventos/StealCoins.java
@@ -3,8 +3,10 @@ package Eventos;
 import Juego.Jmain;
 import Juego.Player;
 import EstructurasDatos.SimpleList;
+import Music.musicstuff;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
@@ -21,6 +23,7 @@ public class StealCoins extends Evento implements ActionListener {
     @Override
     public void evento(Player player ) {
 
+
     }
 
     @Override
@@ -30,7 +33,10 @@ public class StealCoins extends Evento implements ActionListener {
         frmrobar = new JFrame("Robar Monedas");
         frmrobar.setBounds(0, 0, 465, 350);
         frmrobar.setLocationRelativeTo(null);
+        frmrobar.setUndecorated(true);
+        frmrobar.pack();
         frmrobar.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 
         JPanel panel99 = new JPanel();
         panel99.setLayout(null);
@@ -40,8 +46,6 @@ public class StealCoins extends Evento implements ActionListener {
 
         robado1 = new JButton();
         robado1.setBounds(280, 89, 50, 50);
-        ImageIcon bgurl87 = new ImageIcon(getClass().getResource("/Eventos/r1.png"));
-        robado1.setIcon(bgurl87);
         robado1.setBorderPainted(false);
         robado1.setContentAreaFilled(false);
         robado1.setOpaque(false);
@@ -52,8 +56,6 @@ public class StealCoins extends Evento implements ActionListener {
 
         robado2 = new JButton();
         robado2.setBounds(380, 89, 50, 50);
-        ImageIcon bgurl88 = new ImageIcon(getClass().getResource("/Eventos/r2.png"));
-        robado2.setIcon(bgurl88);
         robado2.setBorderPainted(false);
         robado2.setContentAreaFilled(false);
         robado2.setOpaque(false);
@@ -64,8 +66,6 @@ public class StealCoins extends Evento implements ActionListener {
 
         robado3 = new JButton();
         robado3.setBounds(330, 160, 50, 50);
-        ImageIcon bgurl89 = new ImageIcon(getClass().getResource("/Eventos/r3.png"));
-        robado3.setIcon(bgurl89);
         robado3.setBorderPainted(false);
         robado3.setContentAreaFilled(false);
         robado3.setOpaque(false);
@@ -85,13 +85,108 @@ public class StealCoins extends Evento implements ActionListener {
 
         frmrobar.setVisible(true);
 
-
-
+        if(Jmain.getInstance().getPlayerList().getLength() == 4) {
+            if (Jmain.getInstance().getPlayerList().getPos(0) == player) {
+                ImageIcon bgurl87 = new ImageIcon(getClass().getResource("/Eventos/r2.png"));
+                robado1.setIcon(bgurl87);
+                ImageIcon bgurl88 = new ImageIcon(getClass().getResource("/Eventos/r3.png"));
+                robado2.setIcon(bgurl88);
+                ImageIcon bgurl89 = new ImageIcon(getClass().getResource("/Eventos/r4.png"));
+                robado3.setIcon(bgurl89);
+            }
+            if (Jmain.getInstance().getPlayerList().getPos(1) == player) {
+                ImageIcon bgurl87 = new ImageIcon(getClass().getResource("/Eventos/r1.png"));
+                robado1.setIcon(bgurl87);
+                ImageIcon bgurl88 = new ImageIcon(getClass().getResource("/Eventos/r3.png"));
+                robado2.setIcon(bgurl88);
+                ImageIcon bgurl89 = new ImageIcon(getClass().getResource("/Eventos/r4.png"));
+                robado3.setIcon(bgurl89);
+            }
+            if (Jmain.getInstance().getPlayerList().getPos(2) == player) {
+                ImageIcon bgurl87 = new ImageIcon(getClass().getResource("/Eventos/r1.png"));
+                robado1.setIcon(bgurl87);
+                ImageIcon bgurl88 = new ImageIcon(getClass().getResource("/Eventos/r2.png"));
+                robado2.setIcon(bgurl88);
+                ImageIcon bgurl89 = new ImageIcon(getClass().getResource("/Eventos/r4.png"));
+                robado3.setIcon(bgurl89);
+            }
+            if (Jmain.getInstance().getPlayerList().getPos(3) == player) {
+                ImageIcon bgurl87 = new ImageIcon(getClass().getResource("/Eventos/r1.png"));
+                robado1.setIcon(bgurl87);
+                ImageIcon bgurl88 = new ImageIcon(getClass().getResource("/Eventos/r2.png"));
+                robado2.setIcon(bgurl88);
+                ImageIcon bgurl89 = new ImageIcon(getClass().getResource("/Eventos/r3.png"));
+                robado3.setIcon(bgurl89);
+            }
+        }
+        if(Jmain.getInstance().getPlayerList().getLength() == 3) {
+            if (Jmain.getInstance().getPlayerList().getPos(0) == player) {
+                ImageIcon bgurl87 = new ImageIcon(getClass().getResource("/Eventos/r2.png"));
+                robado1.setIcon(bgurl87);
+                ImageIcon bgurl88 = new ImageIcon(getClass().getResource("/Eventos/r3.png"));
+                robado2.setIcon(bgurl88);
+            }
+            if (Jmain.getInstance().getPlayerList().getPos(1) == player) {
+                ImageIcon bgurl87 = new ImageIcon(getClass().getResource("/Eventos/r1.png"));
+                robado1.setIcon(bgurl87);
+                ImageIcon bgurl88 = new ImageIcon(getClass().getResource("/Eventos/r3.png"));
+                robado2.setIcon(bgurl88);
+            }
+            if (Jmain.getInstance().getPlayerList().getPos(2) == player) {
+                ImageIcon bgurl87 = new ImageIcon(getClass().getResource("/Eventos/r1.png"));
+                robado1.setIcon(bgurl87);
+                ImageIcon bgurl88 = new ImageIcon(getClass().getResource("/Eventos/r2.png"));
+                robado2.setIcon(bgurl88);
+            }
+        }
+        if(Jmain.getInstance().getPlayerList().getLength() == 2) {
+            if (Jmain.getInstance().getPlayerList().getPos(0) == player) {
+                ImageIcon bgurl87 = new ImageIcon(getClass().getResource("/Eventos/r2.png"));
+                robado1.setIcon(bgurl87);
+            }
+            if (Jmain.getInstance().getPlayerList().getPos(1) == player) {
+                ImageIcon bgurl87 = new ImageIcon(getClass().getResource("/Eventos/r1.png"));
+                robado1.setIcon(bgurl87);
+            }
+        }
     }
 
 
     @Override
     public void actionPerformed(ActionEvent e) {
 
+        if (Jmain.getInstance().getPlayerList().getLength() == 4) {
+            if (e.getSource() == robado1) {
+
+                frmrobar.setVisible(false);
+            }
+            if (e.getSource() == robado2) {
+                frmrobar.setVisible(false);
+            }
+            if (e.getSource() == robado3) {
+                frmrobar.setVisible(false);
+            }
+
+        }
     }
+
+
+
+
+
+
+
+
+
+
+    public void evento(Player player, Player robado, int monedasrobadas) {
+        if (robado.monedas < monedasrobadas) {
+            player.monedas += robado.monedas;
+            robado.monedas = 0;
+        }else{
+            player.monedas += monedasrobadas;
+            robado.monedas -= monedasrobadas;
+        }
+    }
+
 }
